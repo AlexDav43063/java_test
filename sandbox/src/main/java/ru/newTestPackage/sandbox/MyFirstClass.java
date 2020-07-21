d264ffd62ef02d023a53b81c89332a52c30cfdc1
Aleksandr Davydov <aleksandr.davydov@nordigy.ru>
2020-07-21 17:10:53 +0300

    Lesson about methods, functions and classes

--- sandbox/src/main/java/ru/newTestPackage/sandbox/MyFirstClass.java
+++ sandbox/src/main/java/ru/newTestPackage/sandbox/MyFirstClass.java
@@ -2,7 +2,16 @@ package ru.newTestPackage.sandbox;
 
 public class MyFirstClass {
 
-    public static void main(String[] args) {
-        System.out.println("Hello!!! World!!!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello!!! World!!!");
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата cо cтороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(5,7);
+    System.out.println("Площадь прямоугольника cо cторонами " + r.a + " и " + r.b + " = " + r.area());
+
+  }
+
+
+
 }
