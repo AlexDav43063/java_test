55825b021841d78f08fa3e35ec13188855738418
AlexDav43063 <aleksandr.davydov@nordigy.ru>
2020-08-30 13:22:18 +0300

    les6.11

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,22 +1,41 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
-  @BeforeSuite
-  public void setUp() throws Exception {
-    app.init();
-  }
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-  @AfterSuite
-  public void tearDown() throws Exception {
-    app.stop();
-  }
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] objects) {
+        logger.info("Start '" +m.getName() + "' test, with parameters " + Arrays.asList(objects));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m, Object[] objects) {
+        logger.info("Stop '" +m.getName() + "' test ");
+    }
 
 }
