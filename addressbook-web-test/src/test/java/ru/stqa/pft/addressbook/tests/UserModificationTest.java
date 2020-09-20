4041b46f95fbdda1555e3c843365a0dc4fa89838
AlexDav43063 <aleksandr.davydov@nordigy.ru>
2020-09-20 15:12:04 +0300

    tets3

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
@@ -11,26 +11,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserModificationTest extends TestBase {
 
-//    @BeforeMethod
-//    public void ensurePrecondition() {
-//        if(app.db().groups().size() == 0){
-//            app.goTo().groupPage();
-//            app.group().create(new GroupData().withName("test1"));
-//            app.goTo().homePage();
-//        }
-//        UserData user = new UserData().
-//                withName("Name")
-//                .withMiddleName("MName")
-//                .withLastName("Last Name")
-//                .withNick("Alexxxx")
-//                .withCompany("Company")
-//                .withStreet("13 Elm Street")
-//                .withHome("22")
-//                .withWork("21124").inGroup(new GroupData().withName("test1"));
-//        if (app.db().users().size() == 0) {
-//            app.user().create(user, true);
-//        }
-//    }
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if(app.db().groups().size() == 0){
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1"));
+            app.goTo().homePage();
+        }
+        UserData user = new UserData().
+                withName("Name")
+                .withMiddleName("MName")
+                .withLastName("Last Name")
+                .withNick("Alexxxx")
+                .withCompany("Company")
+                .withStreet("13 Elm Street")
+                .withHome("22")
+                .withWork("21124").inGroup(new GroupData().withName("test1"));
+        if (app.db().users().size() == 0) {
+            app.user().create(user, true);
+        }
+    }
 
     @Test
     public void testUserModification() {
