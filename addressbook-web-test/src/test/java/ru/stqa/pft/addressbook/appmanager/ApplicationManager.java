577405a4c3c46ce2a72130197b73a735912b30bd
AlexDav43063 <aleksandr.davydov@nordigy.ru>
2020-09-20 23:05:56 +0300

    test56789

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -63,7 +63,7 @@ public class ApplicationManager {
     }
 
     public void stop() {
-        wd.findElement(By.linkText("Logout")).click();
+//        wd.findElement(By.linkText("Logout")).click();
         wd.quit();
     }
 
