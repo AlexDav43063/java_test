13646a643adc1c362cc60ba0548c905313beed9b
AlexDav43063 <aleksandr.davydov@nordigy.ru>
2020-09-20 22:36:58 +0300

    test1

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -52,7 +52,7 @@ public class ApplicationManager {
 //            capabilities.setPlatform(Platform.valueOf(System.getProperty("platform", "win10")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
