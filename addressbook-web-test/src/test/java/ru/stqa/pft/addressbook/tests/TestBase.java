18c81feb767a801488c521b828e34ae9e782073f
AlexDav43063 <aleksandr.davydov@nordigy.ru>
2020-09-20 15:05:42 +0300

    test

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,10 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.models.GroupData;
 import ru.stqa.pft.addressbook.models.Groups;
@@ -25,7 +22,7 @@ public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeSuite
+    @BeforeClass
     public void setUp() throws Exception {
         app.init();
     }
